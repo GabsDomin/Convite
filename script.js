d9fb3d33a4f112a36be9c06c5ad5b21fd59ff57e
GabsDomin <GabsDomin@users.noreply.github.com>
2026-05-19 16:53:37 -0300

    Remove gift email field

--- script.js
+++ script.js
@@ -443,10 +443,6 @@ function openGiftModal(gift) {
         <input name="guestName" required autocomplete="name" placeholder="Digite seu nome" />
       </label>
       <label>
-        Seu e-mail <span class="optional-label">opcional</span>
-        <input name="buyerEmail" type="email" autocomplete="email" placeholder="seu@email.com" />
-      </label>
-      <label>
         Mensagem para os noivos <span class="optional-label">opcional</span>
         <textarea name="message" rows="3" placeholder="Escreva uma mensagem com carinho"></textarea>
       </label>
